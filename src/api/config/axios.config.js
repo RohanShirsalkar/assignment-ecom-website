16b18c45fc046519725b89fc836f3a731abee726
Rohan Shirsalkar <95482857+RohanShirsalkar@users.noreply.github.com>
2024-12-24 00:46:56 +0530

    baseUrl updated

--- src/api/config/axios.config.js
+++ src/api/config/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://assignment-ecom-backend-production.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
